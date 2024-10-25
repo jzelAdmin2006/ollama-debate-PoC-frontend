1a8b162b3d5b8f6d1610af86c406aa913d22011f
Johannes Zeller <jo.zeller@protonmail.com>
2024-10-25 12:43:16 +0200

    setup public deployment

--- src/app/services/debate.service.ts
+++ src/app/services/debate.service.ts
@@ -2,12 +2,13 @@ import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { DebateResponse } from '../debate-response';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DebateService {
-  private readonly apiUrl = 'http://localhost:8080/debate';
+  private readonly apiUrl = environment.apiUrl + '/debate';
   private readonly http = inject(HttpClient);
 
   startDebate(input: string, exchanges: number): Observable<DebateResponse[]> {
